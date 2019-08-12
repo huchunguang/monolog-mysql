5fade2a04bb1b16b322877fa37a6cd860a2324a1
huchunguang <huchunguang123@gmail.com>
2019-08-12 18:15:46 +0800

    fixed

--- src/MysqlHandler.php
+++ src/MysqlHandler.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use Illuminate\Config\Repository;
-class MysqlHandler 
+class MysqlHandler
 {
 	protected $table;
 	protected $connection;
@@ -15,18 +15,18 @@ class MysqlHandler
 	public function __construct(Repository $config,$level = Logger::DEBUG, $bubble = true)
 	{
 		$this->config=$config;
-		$this->table      = $this->config('monologDB.DB_LOG_TABLE');
-		$this->connection = $this->config('monologDB.DB_LOG_CONNECTION');
-		parent::__construct($level, $bubble);
+		$this->table      = $this->config['logDB.DB_LOG_TABLE'];
+		
+		$this->connection = $this->config['logDB.DB_LOG_CONNECTION'];
 	}
-	protected function write(array $record)
+	public function write(array $record)
 	{
 		$data = [
 			'host'    => gethostname(),
 			'uri'     => $record['uri'],
 			'params'     => json_encode($record['params'], JSON_UNESCAPED_UNICODE),
 			'raw_return'       => $record['raw_return']??'数据异常',
-			'created_at'  => $record['datetime'],
+			'create_at'  => $record['datetime'],
 		];
 		\Illuminate\Support\Facades\DB::connection($this->connection)->table($this->table)->insert($data);
 	}
